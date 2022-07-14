c6f59e7ecba046e1e636cb54981c9128a67855e7
Sameer <samsatpathyrahul@gmail.com>
2022-07-14 16:22:33 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//testng xml file from maven
+	//how to execute all test cases from test folder- mvn test
+
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+
+}
